a3910f4434ad7262ca830b406d02f0db496cde99
christian_oliveira <christianoliveira1908@hotmail.com>
2021-10-27 22:14:02 -0300

    Inicio da seção sobre Interfaces

---
+++ Interface/Interface.ts
@@ -0,0 +1,14 @@
+interface IEndereco {
+    logradouro: string;
+    numero: string;
+    bairro: string;
+    cidade: string;
+}
+
+const endereco: IEndereco = {
+    logradouro: 'Av. Paulista',
+    numero: "1000",
+    bairro: "Bela vista",
+    cidade: "São Paulo"
+};
+
