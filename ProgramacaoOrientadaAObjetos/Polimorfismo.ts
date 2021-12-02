df77a6cb73ba36b2b4e35609f8b662a37575bd79
christian_oliveira <christianoliveira1908@hotmail.com>
2021-12-01 21:33:24 -0300

    Aula sobre polimorfismo.

---
+++ ProgramacaoOrientadaAObjetos/Polimorfismo.ts
@@ -0,0 +1,23 @@
+class Empresa {
+    prestarServico() {
+        return 'Empresa prestando serviço';
+    }
+}
+
+class Padaria extends Empresa {
+    prestarServico() {
+        return 'Vendendo pão';
+    }
+}
+
+class Merceria extends Empresa {
+    prestarServico(){
+        return 'Vendendo alimentos e bebidas';
+    }
+}
+
+console.log(new Empresa().prestarServico());
+console.log(new Padaria().prestarServico());
+console.log(new Merceria().prestarServico());
+
+
